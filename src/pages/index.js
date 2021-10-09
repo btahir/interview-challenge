52a5b38646833e75ffdea022c36435530b197277
btahir89 <bilal.tahir89@gmail.com>
2021-10-09 15:42:31 -0700

    setup - can see data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,26 @@ import { Box } from 'theme-ui'
 import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { withApollo } from '../apollo/client'
+import { EPOCHES_QUERY } from '../apollo/queries'
 
 const Index = () => {
-	return (
-		<Box>
-			<Box
-				sx={{
-					pt: '48px',
-					m: '0 auto',
-					textAlign: 'center'
-				}}>
-				<h1>Welcome to the Edge & Node coding challenge!</h1>
-			</Box>
-		</Box>
-	)
+  const { data, loading, error } = useQuery(EPOCHES_QUERY)
+
+  console.log(data)
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          pt: '48px',
+          m: '0 auto',
+          textAlign: 'center',
+        }}
+      >
+        <h1>Welcome to the Edge & Node coding challenge!</h1>
+      </Box>
+    </Box>
+  )
 }
 
 export default withApollo(Index, { ssr: false })
-
