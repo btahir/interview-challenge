f5df33046d1ea9d46198b4002a5c281205f81b40
btahir89 <bilal.tahir89@gmail.com>
2021-10-09 18:04:55 -0700

    load table

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Image, Flex } from 'theme-ui'
+import { Box, Image, Flex, Divider } from 'theme-ui'
 import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { withApollo } from '../apollo/client'
@@ -6,8 +6,13 @@ import { EPOCHES_QUERY } from '../apollo/queries'
 
 const Index = () => {
   const { data, loading, error } = useQuery(EPOCHES_QUERY)
+  const [tableData, setTableData] = useState([])
 
-  console.log(data)
+  useEffect(() => {
+    if (data) {
+      setTableData(data.epoches)
+    }
+  })
 
   return (
     <Box
@@ -21,29 +26,64 @@ const Index = () => {
           pt: '48px',
           m: '12px auto',
           width: '90%',
-          // textAlign: 'center',
         }}
       >
         <Flex sx={{ alignItems: 'center' }}>
           <h1>Epochs</h1>
-          <Box
+          <Divider
             sx={{
               border: 'solid',
               borderWidth: '1px',
               height: '24px',
-              width: '1px',
               mx: '3',
-              mb: '4',
+              mb: '6',
               color: 'rgba(255,255,255,0.16)',
             }}
-          ></Box>
-          <Box sx={{ mb: '4', color: '#9ca4ab' }}>
+          />
+          <Box sx={{ mb: '4', color: 'rgba(255,255,255,0.48)' }}>
             <Flex sx={{ alignItems: 'center' }}>
               <Image src="/images/Search.svg" sx={{ mr: '1' }} />
               <div>Search</div>
             </Flex>
           </Box>
         </Flex>
+        <table style={{ marginTop: '20px' }}>
+          <tbody>
+            <tr
+              style={{
+                textTransform: 'uppercase',
+                fontSize: '10px',
+                lineHeight: '12px',
+                letterSpacing: '1.2px',
+                height: '44px',
+              }}
+            >
+              <th>Epoch</th>
+              <th>Start Block</th>
+              <th>End Block</th>
+              <th>Query Fees</th>
+              <th>Total Rewards</th>
+            </tr>
+            {tableData.map((item) => (
+              <tr
+                key={item.id}
+                style={{
+                  height: '64px',
+                  fontSize: '16px',
+                  lineHeight: '24px',
+                  backgroundColor: 'rgba(12,10,29,0)',
+                  color: 'rgba(255,255,255,0.64)',
+                }}
+              >
+                <td>{item.id}</td>
+                <td>#{item.startBlock}</td>
+                <td>#{item.endBlock}</td>
+                <td>{item.queryFeesCollected}</td>
+                <td>{item.totalRewards}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Box>
     </Box>
   )
