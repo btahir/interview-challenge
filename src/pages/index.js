9924f71e8207b393ae403dbbfbbde1524ae1678a
btahir89 <bilal.tahir89@gmail.com>
2021-10-09 17:01:11 -0700

    header setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Box } from 'theme-ui'
+import { Box, Image, Flex } from 'theme-ui'
 import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { withApollo } from '../apollo/client'
@@ -10,15 +10,40 @@ const Index = () => {
   console.log(data)
 
   return (
-    <Box>
+    <Box
+      sx={{
+        height: '900px',
+        maxWidth: '1440px',
+      }}
+    >
       <Box
         sx={{
           pt: '48px',
-          m: '0 auto',
-          textAlign: 'center',
+          m: '12px auto',
+          width: '90%',
+          // textAlign: 'center',
         }}
       >
-        <h1>Welcome to the Edge & Node coding challenge!</h1>
+        <Flex sx={{ alignItems: 'center' }}>
+          <h1>Epochs</h1>
+          <Box
+            sx={{
+              border: 'solid',
+              borderWidth: '1px',
+              height: '24px',
+              width: '1px',
+              mx: '3',
+              mb: '4',
+              color: 'rgba(255,255,255,0.16)',
+            }}
+          ></Box>
+          <Box sx={{ mb: '4', color: '#9ca4ab' }}>
+            <Flex sx={{ alignItems: 'center' }}>
+              <Image src="/images/Search.svg" sx={{ mr: '1' }} />
+              <div>Search</div>
+            </Flex>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   )
